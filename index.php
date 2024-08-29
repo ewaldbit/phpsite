764c94bf9eb2cde1c6bf70862dc42e469efe18bd
Ewald Bitencourt <ewaldbit@gmail.com>
2024-08-28 22:00:06 -0300

    troca banner, section depoimento e serviços

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Projeto Dev Web</title>
+    <title>CallTheProphet</title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" type="text/css" rel="stylesheet">
@@ -39,13 +39,62 @@
         <div class="container chamada-banner">
           <div class="row">
               <div class="col-md-12 text-center" >
-                <h2>Call The Prophet</h2>
-                <p>Encontrando direção, encorajamento, consolo e edificação para atingir o seu propósito!</p>  
-                <button type="button" class="btn btn-success">Cadastro</button>
+                <p>Encontrando direção, encorajamento, consolo e edificação para atingir o seu propósito!</p>
+                <a href="">Saiba Mais!</a>  
               </div><!--col-md-12-->
           </div><!--row-->    
         </div><!--container-->
       </section><!--banner-->
+
+      <section class="cadastro-lead">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h2><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Entre na nossa lista!</h2>
+            </div><!--col-md-6-->
+            <div class="col-md-6">
+              <input type="text" name="nome"><input type="submit" >
+            </div><!--col-md-6-->
+          </div><!--row-->
+        </div><!--container-->
+      </section><!--cadastro-lead-->
+
+      <section class="depoimento text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h2>Depoimento</h2>
+              <blockquote>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac congue neque. Aenean nunc mauris, sagittis gravida blandit id, condimentum et justo. In hac habitasse platea dictumst. Vivamus tristique pellentesque libero, dignissim bibendum leo euismod nec. Aenean commodo euismod ligula, vitae dapibus mauris malesuada et. Nulla in mollis nisi, ac pulvinar odio. Aenean iaculis orci tempus nulla malesuada pulvinar."</p>
+              </blockquote>
+            </div><!--col-md-12-->
+          </div><!--row-->
+        </div><!--container-->
+      </section><!--depoimento-->
+
+      <section class="services text-center">
+        <h2>Como funciona o sistema?</h2>
+        <div class="container services-container">
+          <div class="row">
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <h3><span class="glyphicon glyphicon-user"></span></h3>
+              <h2>Atendimento  Personalizado</h2>
+              <p>Atendimento Online. Onde você estiver, no tempo em que você estiver disponível, 24h diários</p>
+            </div><!--col-md-4-->
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <h3><span class="glyphicon glyphicon-road"></span></h3>
+              <h2>Direcionamentos Proféticos</h2>
+              <p>Atendimento por mensagens de texto, audio ou vídeo chamadas. Receba direcionamentos proféticos</p>
+            </div><!--col-md-4-->
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <h3><span class="glyphicon glyphicon-calendar"></span></h3>
+              <h2>Agenda de Chamadas de Vídeo</h2>
+              <p>Agende suas reuniões com o profeta para organizar seu tempo e não se esquecer do seu comprimisso profético</p>
+            </div><!--col-md-4-->
+          </div><!--row-->
+        </div><!--container-->
+      </section><!--funcionamento-->
+
     </div><!--box-->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
